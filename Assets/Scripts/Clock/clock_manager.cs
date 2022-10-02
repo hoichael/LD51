299aaeac69684b47f2d3a874ce774a195658f109
hoichael <31223078+hoichael@users.noreply.github.com>
2022-10-03 01:47:24 +0200

    Fix time
    - we aint enumerating no more. . . we be invoking repeatedly now 8)

--- Assets/Scripts/Clock/clock_manager.cs
+++ Assets/Scripts/Clock/clock_manager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class clock_manager : MonoBehaviour
@@ -16,25 +14,63 @@ public class clock_manager : MonoBehaviour
     int pendCurrentIteration;
     int pendCurrentIncr; // either 1 or -1 depending on current "direction" of pendulum
 
-    float t0, t1, t2, t3;
+    float t0;
 
     private void Start()
     {
+        //Application.targetFrameRate = 60;
+        t0 = Time.time;
+
         // set initial anchor rotations for fresh clock cycle
         anchorHand.localRotation = Quaternion.Euler(new Vector3(0, 0, currentRotHand));
         //anchorPendulum.localRotation = Quaternion.Euler(new Vector3(0, 0, rotOuterPendulum));
 
-        StartCoroutine(HandRoutine());
+        //StartCoroutine(HandRoutine());
+        InvokeRepeating("HandleHandLol", 1, 1);
 
         //pendAnimnActive = true;
         pendCurrentIncr = 1;
         pendCurrentIteration = Mathf.FloorToInt(pendMoveIterations * 0.5f);
         pendInterval = 10 / pendMoveIterations;
-        StartCoroutine(PendelumRoutine());
+        //StartCoroutine(PendelumRoutine());
+        InvokeRepeating("HandlePendulum", 0, pendInterval);
 
-        t0 = Time.time;
     }
 
+    private void HandlePendulum()
+    {
+        pendCurrentIteration += pendCurrentIncr;
+
+        float t = pendCurrentIteration / pendMoveIterations;
+
+        anchorPendulum.localRotation = Quaternion.Lerp(
+            Quaternion.Euler(new Vector3(0, 0, rotOuterPendulum)),
+            Quaternion.Euler(new Vector3(0, 0, -rotOuterPendulum)),
+            curvePend.Evaluate(t)
+            );
+
+        // if cycle finished flip direction
+        if (pendCurrentIteration >= pendMoveIterations || pendCurrentIteration <= 0)
+        {
+            pendCurrentIncr *= -1;
+            print(Time.time - t0);
+            t0 = Time.time;
+        }
+    }
+
+    private void HandleHandLol()
+    {
+        currentRotHand -= 36;
+        if (currentRotHand == -180)
+        {
+            currentRotHand = 180;
+            //print(Time.time - t0);
+            //t0 = Time.time;
+        }
+        anchorHand.localRotation = Quaternion.Euler(new Vector3(0, 0, currentRotHand));
+    }
+
+    /*
     private IEnumerator PendelumRoutine()
     {
         yield return new WaitForSeconds(pendInterval);
@@ -74,4 +110,5 @@ public class clock_manager : MonoBehaviour
         anchorHand.localRotation = Quaternion.Euler(new Vector3(0, 0, currentRotHand));
         StartCoroutine(HandRoutine());
     }
+    */
 }
