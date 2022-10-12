a38478a6586be0286601e11646996c9af5251a0a
hoichael <31223078+hoichael@users.noreply.github.com>
2022-10-12 21:28:57 +0200

    Create LV_D_0.tmx test level
    - ST2U working fine so far

---
+++ Assets/Imports/Sprites/Tiles/DEV/Metrics/T_metrics.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="T_metrics" tilewidth="8" tileheight="8" tilecount="672" columns="28">
+ <image source="spr_T_metrics.png" width="224" height="192"/>
+</tileset>
