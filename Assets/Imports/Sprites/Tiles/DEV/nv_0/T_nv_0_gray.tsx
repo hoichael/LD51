13b6c6816dcd98b5adae015dbc6bed474b98c191
hoichael <31223078+hoichael@users.noreply.github.com>
2022-10-15 01:58:14 +0200

    Create tsx tilesets from spritesheets + move and rename some files

---
+++ Assets/Imports/Sprites/Tiles/DEV/nv_0/T_nv_0_gray.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="T_nv_0_gray" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="spr_nv_0_gray.png" width="256" height="256"/>
+</tileset>
