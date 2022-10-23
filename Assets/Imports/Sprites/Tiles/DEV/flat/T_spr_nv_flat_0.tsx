dd8568ca7cd7a0dfa4091dd673058eb10a5fe1c4
hoichael <31223078+hoichael@users.noreply.github.com>
2022-10-23 20:24:18 +0200

    Import flat tileset and setup flat test-level

---
+++ Assets/Imports/Sprites/Tiles/DEV/flat/T_spr_nv_flat_0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="T_spr_nv_flat_0" tilewidth="8" tileheight="8" tilecount="24" columns="6">
+ <image source="spr_nv_flat_0.png" width="48" height="32"/>
+</tileset>
