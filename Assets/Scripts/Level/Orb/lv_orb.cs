d1e6d50fe327a9a1fa03be99fbb19b477c6fe92b
hoichael <31223078+hoichael@users.noreply.github.com>
2023-01-05 10:46:21 +0100

    Basic orb pickup anim + move some files

--- Assets/Scripts/Level/Orb/lv_orb.cs
+++ Assets/Scripts/Level/Orb/lv_orb.cs
@@ -6,6 +6,8 @@ public class lv_orb : lv_colhandler_base
 {
     [SerializeField] lv_orb_manager manager;
     [SerializeField] Transform activeContainer;
+    [SerializeField] lv_orb_anim anim;
+    [SerializeField] Collider2D col;
 
     // OLD: OnEnable is triggered on level init because orb container is set active - orb objects themselves ALWAYS stay enabled
     // -> NOW: Reset() is called from lv_orb_manager to allow for reset without disabling level container/orb container
@@ -22,12 +24,20 @@ public class lv_orb : lv_colhandler_base
     public void Reset()
     {
         activeContainer.gameObject.SetActive(true);
+        col.enabled = true;
+        anim.enabled = false;
+        transform.localScale = Vector3.one;
     }
 
     public override void HandleCol()
     {
-        activeContainer.gameObject.SetActive(false);
+        print("asdfsdf");
+        //activeContainer.gameObject.SetActive(false);
         manager.HandleCollect();
+        col.enabled = false;
+        print(anim.enabled);
+        anim.enabled = true;
+        print(anim.enabled);
     }
 
     // called from ed_lv_gen
---
+++ Assets/Scripts/Level/Orb/lv_orb_anim.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lv_orb_anim : MonoBehaviour
+{
+    [SerializeField] float animSpeed;
+    [SerializeField] AnimationCurve animCurve;
+
+    [SerializeField] float currentLerpFactor;
+
+    private void OnEnable()
+    {
+        currentLerpFactor = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        HandleAnim();
+    }
+
+    private void HandleAnim()
+    {
+        currentLerpFactor = Mathf.MoveTowards(currentLerpFactor, 1, animSpeed);
+
+        transform.localScale = Vector3.Lerp(
+            new Vector3(0, 0, 0),
+            new Vector3(2, 2, 2),
+            animCurve.Evaluate(currentLerpFactor)
+            );
+
+        if (currentLerpFactor == 1)
+        {
+            this.enabled = false;
+        }
+    }
+}
